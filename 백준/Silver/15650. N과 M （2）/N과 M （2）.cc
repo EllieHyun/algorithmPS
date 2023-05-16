5d58646a4cd38e1d37ed5bf4d4557ea6e76402aa
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-05-16 16:25:23 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,39 @@
+// n과 m (1)
+#include <iostream>
+#include <algorithm>
+
+#define MAX 9
+using namespace std;
+
+bool visited[MAX];
+int num[MAX];
+int result[MAX];
+int n, m;
+
+void dfs(int start, int cnt) {
+    if (cnt == m) {
+        for(int i=0; i<m; i++) cout << result[i] << " ";
+        cout << "\n";
+    }
+    for (int i = start; i <= n; i++) {
+        if (visited[i]) continue;
+        visited[i] = true;
+        result[cnt] = num[i];
+        dfs(i, cnt + 1);
+        visited[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) num[i] = i;
+
+    dfs(1, 0);
+
+    return 0;
+}
