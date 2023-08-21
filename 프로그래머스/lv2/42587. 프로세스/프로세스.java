13cd1178970e935c134af37ec3fb17f8ee91be93
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-08-21 23:51:20 +0900

    [level 2] Title: 프로세스, Time: 1.05 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Process implements Comparable<Process> {
+    int priority;
+    int index;
+    
+    public Process(int priority, int index) {
+        this.priority = priority;
+        this.index = index;
+    }
+    
+    @Override
+    public int compareTo(Process process) {
+        if(this.priority < process.priority) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {        
+        int answer = 0;
+        PriorityQueue<Process> pq = new PriorityQueue<Process>();
+        Queue<Process> q = new LinkedList<Process>();
+        for(int i=0; i<priorities.length; i++) {
+            Process process = new Process(priorities[i], i);
+            pq.offer(process);
+            q.offer(process);
+        }
+    
+        
+        while(!q.isEmpty()) {
+            Process curProcess = q.poll();
+            Process maxProcess = pq.peek();
+            // System.out.println("cur : " + curProcess.index + ", " + curProcess.priority);
+            // System.out.println("max : " + maxProcess.index + ", " + maxProcess.priority);
+            if(curProcess.priority == maxProcess.priority) {
+                pq.poll();
+                answer++;
+                if(curProcess.index == location) {
+                    return answer;
+                }
+            } else {
+                q.offer(curProcess);
+            }
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
