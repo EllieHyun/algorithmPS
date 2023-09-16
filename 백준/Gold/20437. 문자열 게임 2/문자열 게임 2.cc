cf5a6583715086760f0615ea8770c6eb0a119fdc
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-09-16 12:56:04 +0900

    [Gold V] Title: 문자열 게임 2, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.cc
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.cc
@@ -1,44 +1,41 @@
+// 문자열 게임2
 #include <iostream>
 #include <vector>
-#include <string>
-#include <string.h>
-#include <algorithm>
+
 using namespace std;
- 
-int main(){
-    
-    int T;
-    cin >> T;
-    
-    vector<int> vecList[26];
- 
-    for(int test_case = 1; test_case <=T; test_case++){
-        string W;
-        int K;
-        cin >> W >> K;
-        
-        int mini = 100000;
-        int maxi = 0;
-        for(int i = 0; i<26; i++) vecList[i].clear();
-        for(int i = 0; i<W.length(); i++){
-            int num = W[i] - 97;    // 's' = 18
-            vecList[num].push_back(i);      //해당 문자의 인덱스를 넣음.
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> arr[26];   // 각 알파벳이 시작한 위치를 저장할 위치
+
+    int t, k;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        string w;
+        cin >> w >> k;
+        int minAns = 2e9;
+        int maxAns = -1;
+        for(int j=0; j<26; j++) {
+            arr[j].clear();
         }
-       
-        for(int i = 0; i<26; i++){
-            int vsize = (int)vecList[i].size();
-            if(vsize >= K){ //조건을 만족하는 후보 알파벳들만 고려해서 탐색함으로써 시간 초과 해결.
-                for(int j = 0; j<=vsize - K; j++){  //최소 K개를 포함해야 하므로, 만약 vsize=5이고, K=3이면 j=0,1,2까지만 검사하면 됨.
-                    mini = min(mini, vecList[i][j+K-1] - vecList[i][j] + 1);
-                    maxi = max(maxi, vecList[i][j+K-1] - vecList[i][j] + 1);
+        for (int j = 0; j < w.size(); j++) {
+            arr[w[j] - 'a'].push_back(j);
+        }
+        for (int m = 0; m < 26; m++) {
+            if (arr[m].size() >= k) {
+                // 최소 k개 이상을 가지고 있는 알파벳들
+                for (int a = 0; a <= arr[m].size() - k; a++) {
+                    minAns = min(minAns, arr[m][a + k - 1] - arr[m][a] + 1);
+                    maxAns = max(maxAns, arr[m][a + k - 1] - arr[m][a] + 1);
                 }
             }
         }
-        
-        if(mini == 100000 || maxi == 0) cout << -1 << "\n";
-        else cout << mini << " " << maxi << "\n";
-        
+        if (minAns == 2e9 && maxAns == -1) cout << -1 << "\n";
+        else cout << minAns << " " << maxAns << "\n";
     }
-    
+
     return 0;
 }
