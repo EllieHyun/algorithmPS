bf65a319485292306798f46ec2b76418831172f3
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2024-02-04 15:58:10 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -1,20 +1,35 @@
-#include<iostream>
- 
-long long a[1500050];
-int INF = 1000000;
- 
+// 피보나치 수 3
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+#define INF 1000000
+
+ll n;
+ll arr[1500050];
+
 void fibonacci() {
-    a[0] = 0;
-    a[1] = 1;
+    arr[0] = 0;
+    arr[1] = 1;
+
     for (int i = 0; i < 1500000; i++) {
-        a[i + 2] = (a[i + 1] + a[i]) % INF;
+        arr[i + 2] = (arr[i+1] + arr[i]) % INF;
     }
 }
- 
+
 int main() {
-    long long n;
-    std::cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
     fibonacci();
-    std::cout << a[n%1500000] << "\n";
- 
+
+    cout << arr[n % 1500000] << "\n";
+
+    return 0;
 }
+
